53b2051a8563d431da24ee661fbb6483396d6324
Arnaud Wald <arnaudwald@gmail.com>
2020-06-11 12:40:09 +0200

    Update install.ps1

--- bin/install.ps1
+++ bin/install.ps1
@@ -66,7 +66,8 @@ dl $zipurl $zipfile
 Write-Output 'Extracting...'
 [IO.Compression.ZipFile]::ExtractToDirectory($zipfile, "$dir\_tmp")
 Copy-Item "$dir\_tmp\*-master\*" $dir -Recurse -Force
-Remove-Item "$dir\_tmp", $zipfile -Recurse -Force
+Remove-Item "$dir\_tmp",-Recurse -Force
+Remove-Item $zipfile -Recurse -Force
 
 ensure_robocopy_in_path
 ensure_scoop_in_path
